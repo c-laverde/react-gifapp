bb59bf2d88c31829e97ad7f2e6c8187a1f8836e5
Cristian <cristianlaverde94@gmail.com>
2020-10-09 21:07:18 -0500

    docs add

---
+++ docs/precache-manifest.3ccb0adc55c339f1c41864c92e461827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94b59402d53bdaa5a390fa9f7a6b8d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ba10f68f100b4d9cfbe",
+    "url": "/static/css/main.beee735f.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba10f68f100b4d9cfbe",
+    "url": "/static/js/main.a8f22f5f.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
